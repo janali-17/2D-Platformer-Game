649b910558872639cc9a29e49160a838636a3f2a
Jan Ali Hassan <68240754+janali-17@users.noreply.github.com>
2025-06-21 22:45:36 +0500

    Jump and Crouch anime Input Setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private const string IS_RUNNING = "IsRunning";
+    private const string JUMP = "Jump";
+    private const string CROUCH = "Crouch";
 
     [SerializeField]private Animator animator;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Rigidbody2D rb;
+
+    private float jump;
+    private bool crouch;
+
     void Start()
     {
         
@@ -22,7 +29,18 @@ public class PlayerController : MonoBehaviour
     private void Movement()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        jump = Input.GetAxisRaw("Vertical");
         animator.SetFloat(IS_RUNNING, Mathf.Abs(speed));
+        Jump();
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool(CROUCH, true);
+        }
+        else {
+            animator.SetBool(CROUCH, false);
+
+        }
 
         if (speed < 0f) {
             spriteRenderer.flipX = true;
@@ -32,4 +50,18 @@ public class PlayerController : MonoBehaviour
             spriteRenderer.flipX = false;
         }
     }
+
+    private void Jump()
+    {
+        if (jump > 0)
+        {
+            animator.SetBool(JUMP, true);
+        }
+        else
+        {
+            animator.SetBool(JUMP, false);
+        }
+
+    }
+
 }
